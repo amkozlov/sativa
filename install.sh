9d0a364d310b6f778c2cd8a6958c3c8d7d0d12a8
kozlov <alexey.kozlov@h-its.org>
2014-12-23 17:52:18 +0100

    fix installation

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+cpu_has_feature() {
+    case `uname` in
+        Darwin)
+            SHOW="sysctl machdep.cpu.features"
+            ;;
+        Linux)
+            SHOW="grep flags /proc/cpuinfo"
+            ;;
+    esac
+    $SHOW | grep -qi "$1"
+}
+
+
+
+if cpu_has_feature avx; then
+    export USE_AVX=yes
+fi
+
 cd raxml
 make
 cd ..
