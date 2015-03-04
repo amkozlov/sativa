cd4e6b59d1d83492a2c8b9155c9fdd1c3612d9d8
kozlov <alexey.kozlov@h-its.org>
2015-03-04 14:14:57 +0100

    fix installation error on OSX

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 cpu_has_feature() {
-    case `uname` in
-        Darwin)
-            SHOW="sysctl machdep.cpu.features"
-            ;;
-        Linux)
-            SHOW="grep flags /proc/cpuinfo"
-            ;;
-    esac
+    if [ $DARWIN -eq 1 ]; then
+        SHOW="sysctl machdep.cpu.features"
+    else
+        SHOW="grep flags /proc/cpuinfo"
+    fi
     $SHOW | grep -qi "$1"
 }
 
-
+case `uname` in
+    Darwin)
+        export DARWIN=1
+        ;;
+    Linux)
+        export DARWIN=0
+        ;;
+esac
 
 if cpu_has_feature avx; then
     export USE_AVX=yes
@@ -21,4 +25,4 @@ fi
 cd raxml
 make
 cd ..
-echo "Done!"
+echo "\nDone!"
