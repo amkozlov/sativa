c1c2c234603fdf394f369f7e9639ed09df92a4ac
kozlov <alexey.kozlov@h-its.org>
2016-03-02 14:17:55 +0100

    travis test

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -22,7 +22,6 @@ class ScriptTests(unittest.TestCase):
         self.example_dir = os.path.join(self.sativa_dir, "example")
         self.out_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tmpout")
         os.mkdir(self.out_dir)
-        print "after setup"
     
     def tearDown(self):
         shutil.rmtree(self.out_dir)
@@ -31,7 +30,7 @@ class ScriptTests(unittest.TestCase):
         exec_script = os.path.join(self.sativa_dir, "sativa.py")
         phy_fname = os.path.join(self.testfile_dir, "ref.phy")
         tax_fname = os.path.join(self.testfile_dir, "full.tax")
-        call_str = [exec_script, "-s", phy_fname, "-t", tax_fname, "-n", "testsat", "-x", "BAC", "-o", self.out_dir]
+        call_str = [exec_script, "-s", phy_fname, "-t", tax_fname, "-n", "testsat", "-x", "BAC", "-o", self.out_dir, "-T", "2"]
         try:
             out_str = check_output(call_str, stderr=STDOUT)
         except CalledProcessError as ex:
@@ -46,7 +45,7 @@ class ScriptTests(unittest.TestCase):
         exec_script = os.path.join(self.sativa_dir, "epa_trainer.py")
         ali_fname = os.path.join(self.testfile_dir, "ref.phy")
         tax_fname = os.path.join(self.testfile_dir, "ref.tax")
-        call_str = [exec_script, "-s", ali_fname, "-t", tax_fname, "-n", "testcl", "-x", "BAC", "-o", self.out_dir, "-no-hmmer", "-debug"]
+        call_str = [exec_script, "-s", ali_fname, "-t", tax_fname, "-n", "testcl", "-x", "BAC", "-o", self.out_dir, "-no-hmmer", "-T", "2"]
         try:
 #            out_str = check_output(call_str, stderr=STDOUT)
             call(call_str)
@@ -54,14 +53,12 @@ class ScriptTests(unittest.TestCase):
             print "\n\nCommand line: %s\n\nOutput:\n%s\n" % (ex.cmd, ex.output)
             self.assertTrue(False, msg="Error running epa_trainer.py script")
 
-        print "after trainer call"
-
         refjson_fname = os.path.join(self.out_dir, "testcl.refjson")
         self.assertTrue(os.path.isfile(refjson_fname))
         
         exec_script = os.path.join(self.sativa_dir, "epa_classifier.py")
         query_fname = os.path.join(self.testfile_dir, "ref.phy")
-        call_str = [exec_script, "-r", refjson_fname, "-q", query_fname, "-n", "testcl", "-x", "-o", self.out_dir]
+        call_str = [exec_script, "-r", refjson_fname, "-q", query_fname, "-n", "testcl", "-x", "-o", self.out_dir, "-T", "2"]
         
         try:
 #            out_str = check_output(call_str, stderr=STDOUT)
@@ -69,8 +66,6 @@ class ScriptTests(unittest.TestCase):
         except CalledProcessError as ex:
             print "\n\nCommand line: %s\n\nOutput:\n%s\n" % (ex.cmd, ex.output)
             self.assertTrue(False, msg="Error running epa_classifier.py script")        
-
-        print "after classifier call"
 
         assign_fname = os.path.join(self.out_dir, "testcl.assignment.txt")
         self.assertTrue(os.path.isfile(assign_fname))
